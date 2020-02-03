8276d75bbf7bb458380b989a8c839aa829ccf314
Snir <snikarli@gmail.com>
2020-02-03 17:15:08 +0200

    Fix port issue, added dev script

--- src/app.js
+++ src/app.js
@@ -102,6 +102,6 @@ app.get('*', (req, res) => {
 });
 
 // Telling the server to listen to a cretain port.
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
